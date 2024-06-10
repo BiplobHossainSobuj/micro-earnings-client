785be9f635a0109eaa802844d5496a04eb4967bb
BiploHossainSobuj <bhs.cse@outlook.com>
2024-06-10 21:45:36 +0600

    added password validation

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,8 +3,8 @@ import useAuth from "./useAuth";
 import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
-    // baseURL: 'https://micro-earnings-server.vercel.app',
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://micro-earnings-server.vercel.app',
+    // baseURL: 'http://localhost:5000',
 });
 
 const useAxiosSecure = () => {
