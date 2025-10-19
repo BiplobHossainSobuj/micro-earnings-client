f91655c5d64a248d2db713cd062129db1308ce5f
biplobHossainSobuj <biplobhosainsobuj@gmail.com>
2025-10-19 12:14:10 +0600

    update some placeholder

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -64,7 +64,7 @@ const Register = () => {
                     </label>
                     <label className="input input-bordered flex items-center gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
-                        <input {...register("name", { required: true })} type="text" className="grow" placeholder="Email" />
+                        <input {...register("name", { required: true })} type="text" className="grow" placeholder="name" />
                     </label>
                 </div>
                 <div className="form-control">
@@ -91,7 +91,7 @@ const Register = () => {
                     </label>
                     <label className="input input-bordered flex items-center gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
-                        <input {...register("password", { required: true,pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+/ })} type="text" className="grow" placeholder="Email" 
+                        <input {...register("password", { required: true,pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+/ })} type="text" className="grow" placeholder="password" 
                         aria-invalid={errors.password ? "true" : "false"}/>
                         {errors.password && alert('password must be 6 character with upper and lowercase letter')}
                     </label>
